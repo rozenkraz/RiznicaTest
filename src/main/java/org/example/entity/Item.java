78d34d13f3066cdb37a75b769c5525936a3e9aa5
Jake2Finn <jake.fin@rambler.ru>
2020-07-31 18:38:07 +0300

    1st commit

---
+++ src/main/java/org/example/entity/Item.java
@@ -0,0 +1,16 @@
+package org.example.entity;
+
+
+@Ent
+public class Item {
+
+    private int id;
+    private String name;
+    private String size;
+    private String description;
+    private String photo;
+    private String place;
+    private String shelf;
+    private String itemId;
+
+}
